c4829e4f5ba6c8c6c0ef8c6213032e1bac5a77d7
luis-tolentino <lfotolentino@gmail.com>
2024-03-30 19:06:45 -0400

    added the DriverScript Class and improved the config file

--- src/main/java/com/ws/base/DriverScript.java
+++ src/main/java/com/ws/base/DriverScript.java
@@ -1,5 +1,64 @@
 package com.ws.base;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.time.Duration;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class DriverScript {
 
+	public static WebDriver driver;
+	static Properties prop;
+	
+//	 create a DriverScript Contructor to import the config.properties File/FileInputStream and load it into prop
+	public DriverScript() {
+		
+		File file = new File("./src/test/resources/config/config.properties");
+		try {
+			FileInputStream fis = new FileInputStream(file);
+			prop = new Properties();
+			prop.load(fis);
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+//	create the application initialization method
+	public void initApplication() {
+		String browser = prop.getProperty("browser");
+		if(browser == "chrome") {
+			driver = new ChromeDriver();
+		}
+		else if(browser == "firefox") {
+			driver = new FirefoxDriver();
+		}
+		else if(browser == "edge") {
+			driver = new EdgeDriver();
+		}
+		else {
+			System.out.println("Please check the config.properties file for supported drivers");
+		}
+		driver.manage().window().maximize();
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		getAppUrl();
+	}
+	
+//	create a Method to get the url
+	public void getAppUrl() {
+		String url = prop.getProperty("url");
+		driver.get(url);
+	}
+	
+//	 create a Method to quit the driver.
+	public void quitDriver() {
+		driver.quit();
+	}
 }
