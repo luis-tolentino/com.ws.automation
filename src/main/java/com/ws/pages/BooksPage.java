d81695346586f656dfb817307eea2a56bbe714a0
luis-tolentino <lfotolentino@gmail.com>
2024-04-02 18:27:48 -0400

    junk for reference

--- src/main/java/com/ws/pages/BooksPage.java
+++ src/main/java/com/ws/pages/BooksPage.java
@@ -3,13 +3,16 @@ package com.ws.pages;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 
 import com.ws.base.DriverScript;
 
 public class BooksPage extends DriverScript {
 //***************** Page Elements*********//
 	@FindBy(xpath="//h1[text()='Books']")WebElement booksPageText;
-	
+	@FindBy(name="products-orderby")WebElement booksDropDown;
+	@FindBy(name="products-pagesize")WebElement booksDisplayQuantity;
+	@FindBy(name="products-viewmode")WebElement booksDisplayOrientation;
 	
 	//*************Page initialization****//
 	
@@ -24,5 +27,20 @@ public class BooksPage extends DriverScript {
 	
 	public boolean isBooksPageTextDisplayed() {
 		return booksPageText.isDisplayed();
+	}
+	
+	public void sortBooksByVisibleText(String text) {
+		Select sel = new Select(booksDropDown);
+		sel.selectByVisibleText(text.trim());
+	}
+	
+	public void displayBooksQuantityByVisibleText(String quantity) {
+		Select sel = new Select(booksDisplayQuantity);
+		sel.selectByVisibleText(quantity.trim());
+	}
+	
+	public void viewBooksDisplayOrientationByVisibleText(String orientation) {
+		Select sel = new Select(booksDisplayOrientation);
+		sel.selectByVisibleText(orientation.trim());
 	}
 }
--- src/test/java/com/ws/testcase/TestBooksPage.java
+++ src/test/java/com/ws/testcase/TestBooksPage.java
@@ -4,7 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TestBooksPage extends BaseTest {
-	@Test
+	@Test(priority=1)
 	public void testBooksPage() {
 		logger = report.createTest("Test books Page");
 		homepage.clickLoginLink();
@@ -26,5 +26,80 @@ public class TestBooksPage extends BaseTest {
 		Assert.assertTrue(text.contains("Books"));
 		logger.pass("books page text is present");
 		
+	}
+	
+	@Test(priority=2)
+	public void SortBooksByVisibleText() {
+		logger = report.createTest("Test sorting books by visible text");
+		homepage.clickLoginLink();
+		logger.pass("clicked Log in link");
+		loginpage.enterEmail("dumitest2@gmail.com");
+		logger.pass("entered email");
+		loginpage.enterPassword("test123");
+		logger.pass("entered password");
+		loginpage.clickLoginButton();
+		logger.pass("clicked the login button");
+		boolean isLogoutLinkDisplayed = userhomepage.isLogoutLinkDisplayed();
+		Assert.assertTrue(isLogoutLinkDisplayed);
+		logger.pass("User logged in successfully");
+		userhomepage.clickBooksPageLink();
+		logger.pass("books page link clicked");
+		Assert.assertTrue(booksPage.isBooksPageTextDisplayed());
+		logger.pass("books page Text is visible");
+		String text = booksPage.getBooksPageText();
+		Assert.assertTrue(text.contains("Books"));
+		logger.pass("books page text is present");
+		booksPage.sortBooksByVisibleText("Price: High to Low");
+		logger.pass("books successfully sorted by visible text");
+	}
+	
+	@Test(priority=3)
+	public void testDisplayBooksQuantityByVisibleText() {
+		logger = report.createTest("Test display books quantity by visible text");
+		homepage.clickLoginLink();
+		logger.pass("clicked Log in link");
+		loginpage.enterEmail("dumitest2@gmail.com");
+		logger.pass("entered email");
+		loginpage.enterPassword("test123");
+		logger.pass("entered password");
+		loginpage.clickLoginButton();
+		logger.pass("clicked the login button");
+		boolean isLogoutLinkDisplayed = userhomepage.isLogoutLinkDisplayed();
+		Assert.assertTrue(isLogoutLinkDisplayed);
+		logger.pass("User logged in successfully");
+		userhomepage.clickBooksPageLink();
+		logger.pass("books page link clicked");
+		Assert.assertTrue(booksPage.isBooksPageTextDisplayed());
+		logger.pass("books page Text is visible");
+		String text = booksPage.getBooksPageText();
+		Assert.assertTrue(text.contains("Books"));
+		logger.pass("books page text is present");
+		booksPage.displayBooksQuantityByVisibleText("4");
+		logger.pass("books quantity successfully displayed by visible text");
+	}
+	
+	@Test(priority=4)
+	public void testViewBooksDisplayOrientationByVisibleText() {
+		logger = report.createTest("Test display books quantity by visible text");
+		homepage.clickLoginLink();
+		logger.pass("clicked Log in link");
+		loginpage.enterEmail("dumitest2@gmail.com");
+		logger.pass("entered email");
+		loginpage.enterPassword("test123");
+		logger.pass("entered password");
+		loginpage.clickLoginButton();
+		logger.pass("clicked the login button");
+		boolean isLogoutLinkDisplayed = userhomepage.isLogoutLinkDisplayed();
+		Assert.assertTrue(isLogoutLinkDisplayed);
+		logger.pass("User logged in successfully");
+		userhomepage.clickBooksPageLink();
+		logger.pass("books page link clicked");
+		Assert.assertTrue(booksPage.isBooksPageTextDisplayed());
+		logger.pass("books page Text is visible");
+		String text = booksPage.getBooksPageText();
+		Assert.assertTrue(text.contains("Books"));
+		logger.pass("books page text is present");
+		booksPage.viewBooksDisplayOrientationByVisibleText("List");
+		logger.pass("books orientation successfully displayed by visible text");
 	}
 }
